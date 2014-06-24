039d4d1e03f223b535c1e95907163b4ad8542270
Matthew Farmer <matthew.farmer@icloud.com>
2014-06-24 19:20:13 -0400

    Post resetting the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,39 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624194014) do
+ActiveRecord::Schema.define(version: 20140624220627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: true do |t|
     t.integer  "user_id"
-    t.string   "brand",                          null: false
-    t.string   "condition",                      null: false
-    t.decimal  "original_price",                 null: false
-    t.text     "description",                    null: false
-    t.decimal  "sale_price",                     null: false
-    t.integer  "trade_price",                    null: false
-    t.boolean  "allow_pickup",   default: false, null: false
-    t.decimal  "weight",                         null: false
-    t.string   "size",                           null: false
+    t.string   "brand",                              null: false
+    t.string   "condition",                          null: false
+    t.decimal  "original_price",                     null: false
+    t.text     "description",                        null: false
+    t.decimal  "sale_price",                         null: false
+    t.integer  "trade_price",                        null: false
+    t.boolean  "allow_pickup",       default: false, null: false
+    t.decimal  "weight",                             null: false
+    t.string   "size",                               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",                          null: false
+    t.string   "title",                              null: false
     t.string   "category"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "fname",           null: false
-    t.string   "lname",           null: false
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "fname",               null: false
+    t.string   "lname",               null: false
+    t.string   "username",            null: false
+    t.string   "email",               null: false
+    t.string   "password_digest",     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "token",           null: false
+    t.string   "token",               null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
